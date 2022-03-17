426261f79353d31c2d0394166c2610d8e4adea23
DanirPT <danir.hucaev@gmail.com>
2022-03-17 17:48:08 +0000

    Fix code, delete comments

--- src/java/com/javarush/cryptoanalyser/CryptoLogic.java
+++ src/java/com/javarush/cryptoanalyser/CryptoLogic.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 import java.nio.channels.FileChannel;
 
@@ -24,13 +25,15 @@ public class CryptoLogic {
         String originalFile = scanner.nextLine();
         String encryptedFile = scanner.nextLine();
         int key = scanner.nextInt();
-        /*  C:/Windows/Tasks/Original.txt
-            C:/Windows/Tasks/Encrypted.txt*/
+
         Path path1 = Path.of(originalFile);
         Path path2 = Path.of(encryptedFile);
 
+        if (key < 0){
+            System.out.println("Can not be less than 0");
+        }else {
         StringBuilder builder = new StringBuilder();
-        if (Files.isRegularFile(path1) && Files.isRegularFile(path2)){
+        if (Files.isRegularFile(path1)){
             try(Writer writer = new BufferedWriter(new FileWriter(encryptedFile))) {
                 RandomAccessFile randomAccessFile = new RandomAccessFile(originalFile, "rw");
                 FileChannel channel = randomAccessFile.getChannel();
@@ -47,21 +50,24 @@ public class CryptoLogic {
                     bytesRead = channel.read(buffer);
                 }
                 //encrypting file
-                int i1 =(int) key - ((key / (ALPHABET.length + 1)) * (ALPHABET.length + 1));
+                int i1 =(int) key - ((key / (ALPHABET.length)) * (ALPHABET.length));
 
                 for (int i = 0; i < builder.length(); i++){
                     for (int j = 0; j < ALPHABET.length; j++){
-                        if ( ALPHABET[(char)j] == builder.charAt((char)i)){
-                            if (key > ALPHABET.length) {
-                                if (j + i1 > ALPHABET.length) {
-                                    writer.write(ALPHABET[i1 - (ALPHABET.length - j)]);//от ключа отнять количество пройденных букв
-                                } else {
+
+                        if ( ALPHABET[(char)j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char)i)){
+                            if (key >= ALPHABET.length) {
+                                if (j + i1 > ALPHABET.length-1) {
+                                    writer.write(ALPHABET[i1 - (ALPHABET.length - j)]);
+                                }else if (j + i1 == ALPHABET.length) {
+                                    writer.write(ALPHABET[(char) i1 - 1]);
+                                }else {
                                     writer.write(ALPHABET[(char) (j + i1)]);
                                 }
                             } else {
-                                        if ( ALPHABET[(char)j] == builder.charAt((char)i)){
+                                        if ( ALPHABET[(char)j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char)i)){
                                             if (j+key > ALPHABET.length){
-                                                writer.write(ALPHABET[(char)key - (ALPHABET.length - j)]);//от ключа отнять количество пройденных букв
+                                                writer.write(ALPHABET[(char)key - (ALPHABET.length - j)]);
                                             }else if (j + key == ALPHABET.length){
                                                 writer.write(ALPHABET[(char)key-1]);
                                             }else  {
@@ -77,6 +83,7 @@ public class CryptoLogic {
             }
         }else {
             System.out.println("File name is incorrect or file doesn't exist");
+        }
         }
     }
 }
--- src/java/com/javarush/cryptoanalyser/HackingMessage.java
+++ src/java/com/javarush/cryptoanalyser/HackingMessage.java
@@ -1,5 +1,78 @@
 package com.javarush.cryptoanalyser;
 
-public class HackingMessage {
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Locale;
+import java.util.Scanner;
 
+public class HackingMessage {
+    private static final char[] ALPHABET = {
+            'a','b','c','d','e','f','g','h','i','j','k','l',
+            'm','n', 'o','p','q','r','s','t','u','v','w','x','y',
+            'z','.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '
+    };
+
+    public void unblock(){
+        Scanner scanner = new Scanner(System.in);
+
+        String encryptedFile = scanner.nextLine();
+        String originalFile = scanner.nextLine();
+
+        int key = scanner.nextInt();
+
+        Path path1 = Path.of(originalFile);
+        Path path2 = Path.of(encryptedFile);
+
+        StringBuilder builder = new StringBuilder();
+        if (Files.isRegularFile(path1)){
+            try(Writer writer = new BufferedWriter(new FileWriter(originalFile))) {
+                RandomAccessFile randomAccessFile = new RandomAccessFile(encryptedFile, "rw");
+                FileChannel channel = randomAccessFile.getChannel();
+
+                ByteBuffer buffer = ByteBuffer.allocate(100);
+                int bytesRead = channel.read(buffer);
+
+                while (bytesRead != -1){
+                    buffer.flip();
+                    while (buffer.hasRemaining()){
+                        builder.append((char) buffer.get());
+                    }
+                    buffer.clear();
+                    bytesRead = channel.read(buffer);
+                }
+
+                int i1 =(int) key - ((key / (ALPHABET.length)) * (ALPHABET.length));
+                for (int i = 0; i < builder.length(); i++){
+                    for (int j = 0; j < ALPHABET.length; j++){
+                        if ( ALPHABET[(char)j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char)i)){
+                            if (key >= ALPHABET.length) {
+                                if (j - i1 < 0) {
+                                    writer.write(ALPHABET[(ALPHABET.length-1) - (key-(j+1))]);
+                                } else {
+                                    writer.write(ALPHABET[(char) (j - i1)]);
+                                }
+                            } else {
+                                if ( ALPHABET[(char)j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char)i)){
+                                    if (j-key < 0){
+                                        writer.write(ALPHABET[(ALPHABET.length-1) - (key-(j+1))]);
+                                    }else if (j - key == 0){
+                                        writer.write(ALPHABET[0]);
+                                    }else  if(j - key>0){
+                                        writer.write(ALPHABET[(char)(j - key)]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            System.out.println("File name is incorrect or file doesn't exist");
+        }
+    }
 }
