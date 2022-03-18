26a268e3a1f0e8f2df61f37fccbcfac0310dc656
DanirPT <danir.hucaev@gmail.com>
2022-03-18 02:03:39 +0000

    finished CryptoLogic.java, Made validating of files

--- src/java/com/javarush/cryptoanalyser/CryptoLogic.java
+++ src/java/com/javarush/cryptoanalyser/CryptoLogic.java
@@ -20,70 +20,158 @@ public class CryptoLogic {
              };
 
     public void encryption(){
+        List<String> prohibitedFiles = new ArrayList<>();
+        prohibitedFiles.add(".bash_history");
+        prohibitedFiles.add(".bash_logout");
+        prohibitedFiles.add(".bash_profile");
+        prohibitedFiles.add(".bashrc");
+        prohibitedFiles.add(".gtkrc");
+        prohibitedFiles.add(".login");
+        prohibitedFiles.add(".logout");
+        prohibitedFiles.add(".profile");
+        prohibitedFiles.add(".viminfo");
+        prohibitedFiles.add(".wm_style");
+        prohibitedFiles.add(".Xdefaults & .Xresources");
+        prohibitedFiles.add(".xinitrc");
+        prohibitedFiles.add("/boot/vmlinuz");
+        prohibitedFiles.add("/dev/fd0");
+        prohibitedFiles.add("/dev/fd0H1440");
+        prohibitedFiles.add("/dev/hda");
+        prohibitedFiles.add("/dev/hdc");
+        prohibitedFiles.add("/dev/null");
+        prohibitedFiles.add("/etc/aliases");
+        prohibitedFiles.add("/etc/bashrc");
+        prohibitedFiles.add("/etc/crontab");
+        prohibitedFiles.add("/etc/exports");
+        prohibitedFiles.add("/etc/fstab");
+        prohibitedFiles.add("/etc/group");
+        prohibitedFiles.add("/etc/grub.conf");
+        prohibitedFiles.add("/etc/hosts");
+        prohibitedFiles.add("/etc/hosts.allow");
+        prohibitedFiles.add("/etc/hosts.deny");
+        prohibitedFiles.add("/etc/inittab");
+        prohibitedFiles.add("/etc/issue");
+        prohibitedFiles.add("/etc/lilo.conf");
+        prohibitedFiles.add("/etc/modules.conf");
+        prohibitedFiles.add("/etc/motd");
+        prohibitedFiles.add("/etc/mtab");
+        prohibitedFiles.add("/etc/passwd");
+        prohibitedFiles.add("/etc/printcap");
+        prohibitedFiles.add("/etc/profile");
+        prohibitedFiles.add("/etc/resolv.conf");
+        prohibitedFiles.add("/etc/securetty");
+        prohibitedFiles.add("/etc/termcap");
+        prohibitedFiles.add("/proc/cpuinfo");
+        prohibitedFiles.add("/proc/filesystems");
+        prohibitedFiles.add("/proc/interrupts");
+        prohibitedFiles.add("/proc/ioports");
+        prohibitedFiles.add("/proc/meminfo");
+        prohibitedFiles.add("/proc/modules");
+        prohibitedFiles.add("/proc/mounts");
+        prohibitedFiles.add("/proc/stat");
+        prohibitedFiles.add("/proc/swaps");
+        prohibitedFiles.add("/proc/version");
+        prohibitedFiles.add("/var/log/lastlog");
+        prohibitedFiles.add("/var/log/messages");
+        prohibitedFiles.add("/var/log/wtmp");
+        prohibitedFiles.add("/bin");
+        prohibitedFiles.add("/boot");
+        prohibitedFiles.add("/dev");
+        prohibitedFiles.add("/etc");
+        prohibitedFiles.add("/etc/init.d");
+        prohibitedFiles.add("/etc/profile.d");
+        prohibitedFiles.add("/etc/rc.d");
+        prohibitedFiles.add("/etc/rc.d/init.d");
+        prohibitedFiles.add("/etc/rc.d/rc?.d");
+        prohibitedFiles.add("/etc/skel");
+        prohibitedFiles.add("/etc/X11");
+        prohibitedFiles.add("/home");
+        prohibitedFiles.add("/lib");
+        prohibitedFiles.add("/mnt");
+        prohibitedFiles.add("/proc");
+        prohibitedFiles.add("/root");
+        prohibitedFiles.add("/sbin");
+        prohibitedFiles.add("/tmp");
+        prohibitedFiles.add("/usr");
+        prohibitedFiles.add("/usr/bin");
+        prohibitedFiles.add("/usr/bin/X11");
+        prohibitedFiles.add("/usr/include");
+        prohibitedFiles.add("/usr/share");
+        prohibitedFiles.add("/usr/lib");
+        prohibitedFiles.add("/usr/local/bin");
+        prohibitedFiles.add("/usr/sbin");
+        prohibitedFiles.add("/var");
+
         Scanner scanner = new Scanner(System.in);
 
         String originalFile = scanner.nextLine();
         String encryptedFile = scanner.nextLine();
         int key = scanner.nextInt();
-
+        String[] arrOfOriginal = originalFile.split("/");
+        String[] arrOfEncrypted = encryptedFile.split("/");
         Path path1 = Path.of(originalFile);
         Path path2 = Path.of(encryptedFile);
+        for (String prohibited:prohibitedFiles){
+            if (originalFile.contains(prohibited) || encryptedFile.contains(prohibited)) {
+                System.out.println("You want to change system file");
+                return;
+            }}
+                if (key < 0) {
+                    System.out.println("Can not be less than 0");
+                } else {
+                    StringBuilder builder = new StringBuilder();
+                    if (Files.isRegularFile(path1)) {
+                        try (Writer writer = new BufferedWriter(new FileWriter(encryptedFile))) {
+                            RandomAccessFile randomAccessFile = new RandomAccessFile(originalFile, "rw");
+                            FileChannel channel = randomAccessFile.getChannel();
 
-        if (key < 0){
-            System.out.println("Can not be less than 0");
-        }else {
-        StringBuilder builder = new StringBuilder();
-        if (Files.isRegularFile(path1)){
-            try(Writer writer = new BufferedWriter(new FileWriter(encryptedFile))) {
-                RandomAccessFile randomAccessFile = new RandomAccessFile(originalFile, "rw");
-                FileChannel channel = randomAccessFile.getChannel();
+                            ByteBuffer buffer = ByteBuffer.allocate(100);
+                            int bytesRead = channel.read(buffer);
 
-                ByteBuffer buffer = ByteBuffer.allocate(100);
-                int bytesRead = channel.read(buffer);
-
-                while (bytesRead != -1){
-                    buffer.flip();
-                    while (buffer.hasRemaining()){
-                        builder.append((char) buffer.get());
-                    }
-                    buffer.clear();
-                    bytesRead = channel.read(buffer);
-                }
-                //encrypting file
-                int i1 =(int) key - ((key / (ALPHABET.length)) * (ALPHABET.length));
-
-                for (int i = 0; i < builder.length(); i++){
-                    for (int j = 0; j < ALPHABET.length; j++){
-
-                        if ( ALPHABET[(char)j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char)i)){
-                            if (key >= ALPHABET.length) {
-                                if (j + i1 > ALPHABET.length-1) {
-                                    writer.write(ALPHABET[i1 - (ALPHABET.length - j)]);
-                                }else if (j + i1 == ALPHABET.length) {
-                                    writer.write(ALPHABET[(char) i1 - 1]);
-                                }else {
-                                    writer.write(ALPHABET[(char) (j + i1)]);
+                            while (bytesRead != -1) {
+                                buffer.flip();
+                                while (buffer.hasRemaining()) {
+                                    builder.append((char) buffer.get());
                                 }
-                            } else {
-                                        if ( ALPHABET[(char)j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char)i)){
-                                            if (j+key > ALPHABET.length){
-                                                writer.write(ALPHABET[(char)key - (ALPHABET.length - j)]);
-                                            }else if (j + key == ALPHABET.length){
-                                                writer.write(ALPHABET[(char)key-1]);
-                                            }else  {
-                                                writer.write(ALPHABET[(char)(j + key)]);
+                                buffer.clear();
+                                bytesRead = channel.read(buffer);
+                            }
+
+                            int i1 = (int) key - ((key / (ALPHABET.length)) * (ALPHABET.length));
+
+                            for (int i = 0; i < builder.length(); i++) {
+                                for (int j = 0; j < ALPHABET.length; j++) {
+
+                                    if (ALPHABET[(char) j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char) i)) {
+                                        if (key >= ALPHABET.length) {
+                                            if (j + i1 > ALPHABET.length - 1) {
+                                                writer.write(ALPHABET[i1 - (ALPHABET.length - j)]);
+                                            } else if (j + i1 == ALPHABET.length) {
+                                                writer.write(ALPHABET[(char) i1 - 1]);
+                                            } else {
+                                                writer.write(ALPHABET[(char) (j + i1)]);
+                                            }
+                                        } else {
+                                            if (ALPHABET[(char) j] == builder.toString().toLowerCase(Locale.ROOT).charAt((char) i)) {
+                                                if (j + key > ALPHABET.length) {
+                                                    writer.write(ALPHABET[(char) key - (ALPHABET.length - j)]);
+                                                } else if (j + key == ALPHABET.length) {
+                                                    writer.write(ALPHABET[(char) key - 1]);
+                                                } else {
+                                                    writer.write(ALPHABET[(char) (j + key)]);
+                                                }
                                             }
                                         }
                                     }
                                 }
                             }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println("File name is incorrect or file doesn't exist");
+                    }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+
             }
-        }else {
-            System.out.println("File name is incorrect or file doesn't exist");
         }
-        }
-    }
-}
