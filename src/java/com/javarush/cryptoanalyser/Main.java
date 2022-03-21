3b428c49e02de6cef478e0d7d65258545550b424
DanirPT <danir.hucaev@gmail.com>
2022-03-21 21:14:06 +0000

    Call Dialog.class to start the program

--- src/java/com/javarush/cryptoanalyser/Main.java
+++ src/java/com/javarush/cryptoanalyser/Main.java
@@ -2,7 +2,7 @@ package com.javarush.cryptoanalyser;
 
 public class Main {
     public static void main(String[] args) {
-        CryptoLogic cryptoLogic = new CryptoLogic();
-        cryptoLogic.encryption();
-    }
+        Dialog dialog = new Dialog();
+        dialog.start();
+  }
 }
