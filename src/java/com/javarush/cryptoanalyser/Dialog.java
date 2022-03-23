791c03497f3b843ac64962228e317cb57d9062dd
DanirPT <danir.hucaev@gmail.com>
2022-03-23 01:42:26 +0000

    put "while(true)" and added "try-catch" operation

--- src/java/com/javarush/cryptoanalyser/Dialog.java
+++ src/java/com/javarush/cryptoanalyser/Dialog.java
@@ -1,47 +1,54 @@
 package com.javarush.cryptoanalyser;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Dialog {
-   public static final String CANCEL = "cancel";
-    void start(){
+    public static final String CANCEL = "cancel";
+
+    void start() {
 
         Scanner scanner = new Scanner(System.in);
-        String numberOfFunction;
-        System.out.println("Hello dear user!");
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Choose an option of the program: ");
-        System.out.println("1: Encrypt file.");
-        System.out.println("2: Decrypt file with");
-        System.out.println("3: Decrypt file with brute - force.");
-        System.out.println("4: Decrypt file with static analysis.");
-        System.out.println("For cancel - use command: \"CANCEL\"");
 
-        numberOfFunction = scanner.nextLine();
+        while (true) {
 
-        if ((numberOfFunction).equalsIgnoreCase(CANCEL)){
-            System.out.println("closed");
-            return;
-        }
-        switch (Integer.parseInt(numberOfFunction)) {
+            System.out.println("Hello dear user!");
+            System.out.println("Choose an option of the program: ");
+            System.out.println("1: Encrypt file.");
+            System.out.println("2: Decrypt file with");
+            System.out.println("3: Decrypt file with brute - force.");
+            System.out.println("4: Decrypt file with static analysis.");
+            System.out.println("For cancel - use command: \"CANCEL\"");
+            try {
 
-            case 1 -> {
-                CryptoLogic cryptoLogic = new CryptoLogic();
-                cryptoLogic.encryption();
-            }
-            case 2 -> {
-                HackingMessage hackingMessage = new HackingMessage();
-                hackingMessage.unblock();
-            }case 3 -> {
-                BruteForce bruteForce = new BruteForce();
-                bruteForce.unblock();
-            }case 4 -> {
-                StaticAnalysis staticAnalysis = new StaticAnalysis();
-                staticAnalysis.checkSimilarText();
+                String numberOfFunction = scanner.nextLine();
+
+                if ((numberOfFunction).equalsIgnoreCase(CANCEL)) {
+                    System.out.println("closed");
+                    return;
+                }
+
+                switch (Integer.parseInt(numberOfFunction)) {
+
+                    case 1 -> {
+                        CryptoLogic cryptoLogic = new CryptoLogic();
+                        cryptoLogic.encryption();
+                    }
+                    case 2 -> {
+                        HackingMessage hackingMessage = new HackingMessage();
+                        hackingMessage.unblock();
+                    }
+                    case 3 -> {
+                        BruteForce bruteForce = new BruteForce();
+                        bruteForce.unblock();
+                    }
+                    case 4 -> {
+                        StaticAnalysis staticAnalysis = new StaticAnalysis();
+                        staticAnalysis.checkSimilarText();
+                    }
+                }
+            } catch (NumberFormatException e){
+                System.out.println("You used wrong command");
             }
         }
     }
